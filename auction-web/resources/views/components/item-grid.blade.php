814ada45bdf3717ab9978cc7daa31ef69e27fba2
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-11-28 17:53:26 +0700

    pusing astaga

---
+++ auction-web/resources/views/components/item-grid.blade.php
@@ -0,0 +1,369 @@
+<div class="xl:w-1/3 md:w-1/2 p-4">
+    <div class="
+            c-card
+            block
+            bg-white
+            shadow-md
+            hover:shadow-xl
+            rounded-lg
+            overflow-hidden
+        ">
+        <img class="
+                lg:h-60
+                xl:h-56
+                md:h-64
+                sm:h-72
+                xs:h-72
+                h-72
+                rounded
+                w-full
+                object-cover object-center
+                mb-4
+            " src="https://picsum.photos/720/400" alt="Image Size 720x400" />
+        <div class="p-4">
+            <h3 class="
+                    tracking-widest
+                    text-indigo-500 text-xs
+                    font-medium
+                    title-font
+                ">
+                tipe barang.
+            </h3>
+            <h2 class="
+                    text-lg text-gray-900
+                    font-medium
+                    title-font
+                    mb-4
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                NAMA BARANG.
+            </h2>
+            <p class="
+                    text-gray-600
+                    font-light
+                    text-md
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                deskripsi barang.
+                <br />
+                <a class="inline-flex text-indigo-500" href="#">Read More</a>
+            </p>
+
+            <div class="py-4 border-t border-b text-xs text-gray-700">
+                <div class="grid grid-cols-6 gap-1">
+                    <div class="col-span-2">
+                        Bidder:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-red-400
+                                rounded-full
+                            ">100</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Open Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-green-400
+                                rounded-full
+                            ">Rp. 1000</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Higest Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-blue-400
+                                rounded-full
+                            ">Rp. 8214921</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center mt-2">
+                <img class="w-10 h-10 object-cover rounded-full" alt="User avatar" src="https://i.pravatar.cc/200" />
+
+                <div class="pl-3">
+                    <div class="font-medium">Auctioner</div>
+                    <div class="text-gray-600 text-sm">
+                        Jabatan dia
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="xl:w-1/3 md:w-1/2 p-4">
+    <div class="
+            c-card
+            block
+            bg-white
+            shadow-md
+            hover:shadow-xl
+            rounded-lg
+            overflow-hidden
+        ">
+        <img class="
+                lg:h-60
+                xl:h-56
+                md:h-64
+                sm:h-72
+                xs:h-72
+                h-72
+                rounded
+                w-full
+                object-cover object-center
+                mb-4
+            " src="https://picsum.photos/720/400" alt="Image Size 720x400" />
+        <div class="p-4">
+            <h3 class="
+                    tracking-widest
+                    text-indigo-500 text-xs
+                    font-medium
+                    title-font
+                ">
+                tipe barang.
+            </h3>
+            <h2 class="
+                    text-lg text-gray-900
+                    font-medium
+                    title-font
+                    mb-4
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                NAMA BARANG.
+            </h2>
+            <p class="
+                    text-gray-600
+                    font-light
+                    text-md
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                deskripsi barang.
+                <br />
+                <a class="inline-flex text-indigo-500" href="#">Read More</a>
+            </p>
+
+            <div class="py-4 border-t border-b text-xs text-gray-700">
+                <div class="grid grid-cols-6 gap-1">
+                    <div class="col-span-2">
+                        Bidder:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-red-400
+                                rounded-full
+                            ">100</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Open Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-green-400
+                                rounded-full
+                            ">Rp. 1000</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Higest Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-blue-400
+                                rounded-full
+                            ">Rp. 8214921</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center mt-2">
+                <img class="w-10 h-10 object-cover rounded-full" alt="User avatar" src="https://i.pravatar.cc/200" />
+
+                <div class="pl-3">
+                    <div class="font-medium">Auctioner</div>
+                    <div class="text-gray-600 text-sm">
+                        Jabatan dia
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="xl:w-1/3 md:w-1/2 p-4">
+    <div class="
+            c-card
+            block
+            bg-white
+            shadow-md
+            hover:shadow-xl
+            rounded-lg
+            overflow-hidden
+        ">
+        <img class="
+                lg:h-60
+                xl:h-56
+                md:h-64
+                sm:h-72
+                xs:h-72
+                h-72
+                rounded
+                w-full
+                object-cover object-center
+                mb-4
+            " src="https://picsum.photos/720/400" alt="Image Size 720x400" />
+        <div class="p-4">
+            <h3 class="
+                    tracking-widest
+                    text-indigo-500 text-xs
+                    font-medium
+                    title-font
+                ">
+                tipe barang.
+            </h3>
+            <h2 class="
+                    text-lg text-gray-900
+                    font-medium
+                    title-font
+                    mb-4
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                NAMA BARANG.
+            </h2>
+            <p class="
+                    text-gray-600
+                    font-light
+                    text-md
+                    whitespace-nowrap
+                    truncate
+                    ...
+                ">
+                deskripsi barang.
+                <br />
+                <a class="inline-flex text-indigo-500" href="#">Read More</a>
+            </p>
+
+            <div class="py-4 border-t border-b text-xs text-gray-700">
+                <div class="grid grid-cols-6 gap-1">
+                    <div class="col-span-2">
+                        Bidder:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-red-400
+                                rounded-full
+                            ">100</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Open Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-green-400
+                                rounded-full
+                            ">Rp. 1000</span>
+                    </div>
+
+                    <div class="col-span-2">
+                        Higest Bid:
+                        <span class="
+                                inline-flex
+                                items-center
+                                justify-center
+                                px-2
+                                py-1
+                                text-xs
+                                font-bold
+                                leading-none
+                                text-white
+                                bg-blue-400
+                                rounded-full
+                            ">Rp. 8214921</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center mt-2">
+                <img class="w-10 h-10 object-cover rounded-full" alt="User avatar" src="https://i.pravatar.cc/200" />
+
+                <div class="pl-3">
+                    <div class="font-medium">Auctioner</div>
+                    <div class="text-gray-600 text-sm">
+                        Jabatan dia
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- auction-web/routes/web.php
+++ auction-web/routes/web.php
@@ -2,25 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home',
+        'description' => 'This is the home page',
+        'keywords' => 'home, page',
+    ]);
+});
+
+
+Route::get('/add', function () {
+    return view('add', [
+        'title' => 'ADD ITEM',
+        'description' => 'This is the about page',
+        'keywords' => 'about, page',
+    ]);
 });
 
 Route::get('/item/{id}', function ($id) {
     return view('item', ['id' => $id]);
 });
 
+Route::get('/item-edit/{id}', function ($id) {
+    return view('item-edit', ['id' => $id]);
+});
+
 Route::get('/user/{id}', function ($user) {
     return view('user', ['user' => $user]);
+});
+
+Route::get('/login', function () {
+    return view('login', [
+        'title' => 'Login',
+        'description' => 'This is the login page',
+        'keywords' => 'login, page',
+    ]);
+});
+
+Route::get('/register', function () {
+    return view('register', [
+        'title' => 'Register',
+        'description' => 'This is the register page',
+        'keywords' => 'register, page',
+    ]);
 });
