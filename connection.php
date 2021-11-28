28b10322fbff8a450b6bd41240dcaee5f84a6e77
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-11-29 01:50:24 +0700

    fix(database) : fixing database connection to heroku

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = new mysqli("localhost","root","","mahasiswa");
+    $conn = new mysqli("us-cdbr-east-04.cleardb.com","b20e8eb4d582ad","61c08e39","heroku_b3e363666bdcd1c");
     mysqli_connect_errno();
 ?>
