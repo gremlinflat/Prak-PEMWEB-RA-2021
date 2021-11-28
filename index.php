6156a1f28a9a16f07eff0a504db84a731b489d18
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-11-29 01:37:11 +0700

    fix (deploy)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TUGAS PRAKTIKUM 4 - Fahri Novaldi</title>
+    <title>Database Mahasiswa | Praktikum Web 7 | Fahri Novaldi</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <script src="src/jquery-3.5.1.min.js"></script>
